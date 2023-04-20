464fae8385547a648b74416a0ea53bba05b77a43
quimaguirre <quim.aguirre@hotmail.com>
2023-04-20 18:32:30 -0400

    Comparison of alphas

--- scripts/parameters_analysis_diff_coexpr.sh
+++ scripts/parameters_analysis_diff_coexpr.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-folder="GSE193677-inflamed___GSE193677-noninflamed" #"TCGA-BRCA_female___TCGA-Breast_female"
+folder="GSE193677-Rectum_inflamed___GSE193677-Rectum_control" #"TCGA-BRCA_female___TCGA-Breast_female"
 types=("all")
 #sizes=(20 40 60 80 100)
-sizes=(20 100 200 300 400 500 600 700)
+#sizes=(20 100 200 300 400 500 600 700)
+sizes=(20 60 100 140 180 220)
 cats=("common" "disease-gene" "disease-specific" "normal-specific" "undefined")
 for size in ${sizes[@]};
 do
 for type in ${types[@]};
 do
 sbatch --export=size=$size,type=$type,folder=$folder /home/j.aguirreplans/Projects/Scipher/SampleSize/scripts/calculate_separation.sh
-#for cat in ${cats[@]};
-#do
-#sbatch --export=size=$size,type=$type,cat=$cat,folder=$folder /home/j.aguirreplans/Projects/Scipher/SampleSize/scripts/calculate_go_enrichment.sh
-#sbatch --export=size=$size,type=$type,cat=$cat,folder=$folder /home/j.aguirreplans/Projects/Scipher/SampleSize/scripts/calculate_go_enrichment_coexpr.sh
-#sbatch --export=size=$size,type=$type,cat=$cat,folder=$folder /home/j.aguirreplans/Projects/Scipher/SampleSize/scripts/calculate_proximity_multiple_drugs.sh
+for cat in ${cats[@]};
+do
+sbatch --export=size=$size,type=$type,cat=$cat,folder=$folder /home/j.aguirreplans/Projects/Scipher/SampleSize/scripts/calculate_go_enrichment.sh
+sbatch --export=size=$size,type=$type,cat=$cat,folder=$folder /home/j.aguirreplans/Projects/Scipher/SampleSize/scripts/calculate_go_enrichment_coexpr.sh
+sbatch --export=size=$size,type=$type,cat=$cat,folder=$folder /home/j.aguirreplans/Projects/Scipher/SampleSize/scripts/calculate_proximity_multiple_drugs.sh
 #done
 done
 done
