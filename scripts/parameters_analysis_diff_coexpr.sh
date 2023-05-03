704a3acbc73ec17bb5b517cc20296f4bd3a47868
quimaguirre <quim.aguirre@hotmail.com>
2023-05-03 16:49:41 -0400

    Modification of calculation of SD, mean and number of genes / samples for each dataset

--- scripts/parameters_analysis_diff_coexpr.sh
+++ scripts/parameters_analysis_diff_coexpr.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-folder="GSE193677-Rectum_inflamed___GSE193677-Rectum_control" #"TCGA-BRCA_female___TCGA-Breast_female"
+folder="GSE193677-Rectum_CD_inflamed___GSE193677-Rectum_Control_noninflamed" #"TCGA-BRCA_female___TCGA-Breast_female"
 types=("all")
-#sizes=(20 40 60 80 100)
-#sizes=(20 100 200 300 400 500 600 700)
-sizes=(20 60 100 140 180 220)
+sizes=(20 40 60 80 100) # GSE193677-Rectum_CD_inflamed___GSE193677-Rectum_Control_noninflamed
+#sizes=(20 60 100 140 180 220) # GSE193677-Rectum_inflamed___GSE193677-Rectum_control
 cats=("common" "disease-gene" "disease-specific" "normal-specific" "undefined")
 for size in ${sizes[@]};
 do
@@ -15,6 +14,6 @@ do
 sbatch --export=size=$size,type=$type,cat=$cat,folder=$folder /home/j.aguirreplans/Projects/Scipher/SampleSize/scripts/calculate_go_enrichment.sh
 sbatch --export=size=$size,type=$type,cat=$cat,folder=$folder /home/j.aguirreplans/Projects/Scipher/SampleSize/scripts/calculate_go_enrichment_coexpr.sh
 sbatch --export=size=$size,type=$type,cat=$cat,folder=$folder /home/j.aguirreplans/Projects/Scipher/SampleSize/scripts/calculate_proximity_multiple_drugs.sh
-#done
+done
 done
 done
