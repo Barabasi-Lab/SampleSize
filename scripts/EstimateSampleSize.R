cf63b0970cbba441f90be9473729de33d60d3567
quimaguirre <quim.aguirre@hotmail.com>
2024-01-31 18:12:32 -0500

    reorganization of files

--- scripts/EstimateSampleSize.R
+++
@@ -1,8 +0,0 @@
-
-
-#### https://www.youtube.com/watch?v=WVHeX5La2tg&ab_channel=ResearchTips
-#### COMPARING MEANS BETWEEN TWO DIFFERENT GROUPS ####
-power.t.test(delta=0.2, sd=0.5, power=0.8)
-
-#### COMPARING TWO PROPORTIONS ####
-power.prop.test(p1=0.6, p2=0.5, power=0.8)
