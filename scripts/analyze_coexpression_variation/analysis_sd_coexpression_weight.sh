25d2b5d5772b21e202e6d6d4d838e2a5952f341d
quimaguirre <quim.aguirre@hotmail.com>
2024-09-29 11:44:27 -0400

    rename analysis_individual_edges.sh to analysis_sd_coexpression_weight.sh using the same name as the R script that we run

--- scripts/analyze_coexpression_variation/analysis_sd_coexpression_weight.sh
+++ scripts/analyze_coexpression_variation/analysis_sd_coexpression_weight.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+#SBATCH --nodes=2
+#SBATCH --mem=200000
+#SBATCH --job-name=analysis_coexpression_weight
+#SBATCH --time=24:00:00
+#SBATCH -o /home/j.aguirreplans/Projects/Scipher/SampleSize/scripts/../logs/%j_merge_networks_strong_corr.sh.out
+#SBATCH -e /home/j.aguirreplans/Projects/Scipher/SampleSize/scripts/../logs/%j_merge_networks_strong_corr.sh.err
+#SBATCH -p short
+
+
+#!/bin/bash
 #SBATCH --mem=200000
 #SBATCH -p short
 #SBATCH --time=24:00:00
 #SBATCH -o /home/j.aguirreplans/Projects/Scipher/SampleSize/logs/%j_analysis_individual_edges.sh.out
 #SBATCH -e /home/j.aguirreplans/Projects/Scipher/SampleSize/logs/%j_analysis_individual_edges.sh.err
+
+
 module load R/4.0.3
 #Rscript /home/j.aguirreplans/Projects/Scipher/SampleSize/scripts/analysis_individual_edges.R -n /scratch/j.aguirreplans/Scipher/SampleSizeProject/networks_gtex/reads/gtex_Whole.Blood_pearson_combined.txt -o /home/j.aguirreplans/Projects/Scipher/SampleSize/data/out/plots -f gtex_Whole.Blood_pearson
 #Rscript /home/j.aguirreplans/Projects/Scipher/SampleSize/scripts/analysis_individual_edges.R -n /scratch/j.aguirreplans/Scipher/SampleSizeProject/networks_scipher/reads/scipher_scipher.complete.dataset_pearson_combined.txt -o /home/j.aguirreplans/Projects/Scipher/SampleSize/data/out/plots -f scipher_scipher.complete.dataset_pearson
